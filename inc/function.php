35c7b32a246e2173ddf8c7b2fff9bb47b02570f0
Muhammad Hannan <mdhannan.info@gmail.com>
2023-07-08 21:53:42 +0600

    user object and metod

---
+++ inc/function.php
@@ -0,0 +1,18 @@
+<?php 
+
+class Calculate{
+    
+    public function add($number1, $number2)
+    {
+        echo "Summation = " .($number1 + $number2)."<br/>"; 
+    }
+
+
+    public function minus($number1, $number2)
+    {
+        echo "Minus = " .($number1 - $number2)."<br/>"; 
+    }
+
+}
+
+?> 
--- index.php
+++ index.php
@@ -1,26 +1,31 @@
 <?php include './inc/header.php'; ?>
-   <?php 
-    class Person
-    {
-        public $name;
-        public $age;
+<?php include './inc/function.php'; ?>
+   
+    <div class="calculation-div">
+        <form action="" method="post">
+            Number one: <input type="number" name="number1"> <br>
+            
+            Number two: <input type="number" name="number2" style="margin-top:2px;"> <br>
 
-        public function personName(){
-            echo 'My name is'. $this->name . "<br/>";
+            <button type="submit" name="calculate">Calculate</button>
+        </form>
+    </div>
+    <?php 
+        if(isset($_POST['calculate'])){
+            $number1 = $_POST['number1'];
+            $number2 = $_POST['number2'];
+
+            if (empty($number1) || empty($number2)) {
+               echo "<span style='color:#FF0000'>Field must not be empty</span>";
+            }else{
+                echo "Number 1 = " . $number1 ."<br/>";
+                echo "Number 2 = " . $number2 ."<br/>";
+
+                $newCal = new Calculate;
+                $newCal->add($number1, $number2); 
+                $newCal->minus($number1, $number2); 
+            }
         }
+    ?>
 
-        public function age($ageValue){
-            echo 'My age is '. $this->age = $ageValue;
-        }
-
-
-    }
-    
-    $personClassObj = new Person;
-    $personClassObj->name = 'John Doe';
-    $personClassObj->personName();
-    $personClassObj->age('40');
-
-
-   ?> 
 <?php include './inc/footer.php';  ?> 
