26caf47d2ffb766f4938c500949519ce2a882830
Muhammad Hannan <mdhannan.info@gmail.com>
2023-07-12 22:59:27 +0600

    day fouyr

--- __index.php
+++ __index.php
@@ -5,27 +5,29 @@
         public $name;
         public $age;
         public $id;
-        
+
         function __construct($name, $age) {
             $this->name = $name;
             $this->age = $age;
         }
 
-        public function setId($id)
-        {
+        public function setId($id){
             $this->id = $id;
         }
 
         public function __destruct() {
             if (!empty($this->id)) {
-                echo "Destroying by using __destruct magic method";
+                echo "Destroying ";
             }
         }
+
        
     }
     
     $personClassObj = new Person('John Doe', '30');
     $personClassObj->setId(12);
     unset($personClassObj);
+
+   
    ?> 
 <?php include './inc/footer.php';  ?> 
